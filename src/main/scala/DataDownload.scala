fe0fa2bb3046dafead0cefda610cf086d7bb7b5f
Graham Crowell <graham.crowell@gmail.com>
2017-04-29 18:04:56 -0700

    get daily prices, print most recent day, and get open of that day

--- src/main/scala/DataDownload.scala
+++ src/main/scala/DataDownload.scala
@@ -1,6 +1,7 @@
 import java.util.Calendar
 
 import yahoofinance.YahooFinance
+import yahoofinance.histquotes.Interval
 
 /**
   * see http://alvinalexander.com/scala/how-to-write-scala-http-get-request-client-source-fromurl#setting-the-timeout-while-using-scala-io-source-fromurl
@@ -57,10 +58,22 @@ object DataDownload {
   def yahoolib(): Unit = {
     val stock = YahooFinance.get("INTC")
     println(stock.print())
-    var calendar = Calendar.getInstance()
-    calendar.set(1900, 1, 1)
-    val data = stock.getHistory(calendar)
-    println(data)
+    var start = Calendar.getInstance()
+    start.set(1900, 1, 1)
+    val monthlyData = stock.getHistory(start)
+    println(monthlyData)
+    println(monthlyData.get(0))
+
+    var end = Calendar.getInstance().getTime
+    var interval = Interval.DAILY
+    println(interval)
+    val dailyData = stock.getHistory(start, interval)
+    println(dailyData)
+    println(dailyData.get(0))
+    val dailyPriceData = dailyData.get(0)
+    val open = dailyPriceData.open()
+    println(open)
+
 
   }
 
