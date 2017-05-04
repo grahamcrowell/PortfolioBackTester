dcd249007e275253606eaca73287046c2c7b41c4
Graham Crowell <graham.crowell@gmail.com>
2017-05-03 19:06:09 -0700

    clean up

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -44,74 +44,17 @@ object Tests {
     val data = DataDownload.yahoolib()
   }
 
+
+  //  http://queirozf.com/entries/slick-3-reference-and-examples
   def QueryDb: Unit = {
     val db = Database.forConfig("aws")
     val stock = TableQuery[Stock]
-    val session = db.createSession()
-
-    println(session.conn.toString())
-//    val q = stock.map(_.name)
 
     Await.result(
       db.run(stock.map(_.name).result).map { res =>
-        // res is a Vector[String]
         println(res)
       }, Duration.Inf)
-
-
-    def find(symbol: String) = db.run(stock.filter(_.symbol === symbol).result.headOption)
-    val res = find("AA")
-    res.onComplete { case row => println(row.get)}
-    println(find("AA"))
-
-
-    def userNameByIDRange(symbol: Rep[String]) =
-      for {
-        u <- stock if u.symbol === symbol
-      } yield u.name
-
-    val userNameByIDRangeCompiled = Compiled(userNameByIDRange _)
-
-    val namesAction1 = userNameByIDRangeCompiled("MSFT")
-
-    def getNames(id: Int) = db.run(
-      sql"SELECT company_name FROM stock WHERE user_id = 'MSFT'"
-        .as[(String)].headOption)
-
-    println(getNames(123))
-    println(getNames(123).value)
-
-
-    val q = for (c <- stock) yield c.exchange
-    val a = q.result
-    val f: Future[Seq[Option[String]]] = db.run(a)
-
-    f.onSuccess { case s => println(s"Result: ${s}") }
-
-    def foo(): Unit = {
-      println("fuck u slick")
-      db.run(stock.result).map(_.foreach {
-        case (company_name) =>
-          println("  " + company_name)
-      })
-      import ExecutionContext.Implicits.global
-      val s = "Hello"
-      val f: Future[String] = Future {
-        s + " future!"
-      }
-      f foreach {
-        msg => println(msg)
-      }
-    }
-    foo()
-    session.close()
   }
-
-  //
-  //  def UpdateData(): Unit = {
-  //    val data = PriceIO.uploadPriceData("MSFT")
-  //
-  //  }
 }
 
 object Main {
