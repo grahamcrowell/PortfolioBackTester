a7dfca47684e2325d4b0d04ea928736f46834345
Graham Crowell <gcrowell@YVR-NBM0549.local>
2017-05-28 17:18:01 -0700

    getOutDated

--- src/main/scala/org/fantastic/PortfolioBackTester/DataAccessLayer.scala
+++ src/main/scala/org/fantastic/PortfolioBackTester/DataAccessLayer.scala
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 abstract class UpdateTaskSpec
 
-case class UpdatePriceTaskSpec(symbol: String, fromDate: Calendar) extends UpdateTaskSpec
+case class UpdatePriceTaskSpec(symbol: String, fromDate: Option[Int]) extends UpdateTaskSpec
 
 trait Source {
   def getExpectedMostRecentDate(): Calendar
@@ -47,29 +47,37 @@ case object PriceSync {
     else 0
   }
 
-//  def getOutDated(): Seq[UpdatePriceTaskSpec] = {
-//    val db = Database.forConfig("aws")
-//    val stock = TableQuery[Stock]
-//    val price = TableQuery[StockOHLC]
-//
-//    val leftOuterJoinGrouped = (for {
-//      (s, p) <- stock joinLeft price on (_.symbol === _.stock_symbol)
-//    } yield (s.symbol, p.map(_.date_id)))
+  def getOutDated(): Seq[UpdatePriceTaskSpec] = {
+    val db = Database.forConfig("aws")
+    val stock = TableQuery[Stock]
+    val price = TableQuery[StockOHLC]
+    val epoch = 19000101
+
+    val leftOuterJoin = (for {
+      (s, p) <- stock joinLeft price on (_.symbol === _.stock_symbol)
+    } yield (s.symbol, p.map(_.date_id))).groupBy(_._1)
+
+    val leftOuterJoinGroup = leftOuterJoin.map {
+      case (s, p) => (s, p.map {
+        _._2.getOrElse(epoch)
+      }.max)
+    }
 
 
-//    val leftOuterJoinAgg = leftOuterJoinGrouped.map {
-//      case (s, p) => (s, p.)
-//    }
 
-//
-//    Await.result(
-//      db.run(
-//        leftOuterJoinAgg.result).map { res => println(res) }, Duration.Inf
-//    )
 
-//    Seq[UpdatePriceTaskSpec]()
+    // wait for query results to return
+    val result = Await.result(
+      db.run(leftOuterJoinGroup.result)
+      , Duration.Inf
+    )
 
-//  }
+    result.map {
+      case (s, p) => UpdatePriceTaskSpec(s, p)
+    }
+
+  }
+
 
 
 }
