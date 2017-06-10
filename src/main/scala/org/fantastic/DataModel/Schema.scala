4ec760dbc9ce6e94b3d4c4a434a5da30b66fac18
Graham Crowell laptop <graham.crowell@visiercorp.com>
2017-06-10 13:43:33 -0700

    HttpDownloader

--- src/main/scala/org/fantastic/DataModel/Schema.scala
+++ src/main/scala/org/fantastic/DataModel/Schema.scala
@@ -6,26 +6,14 @@ import slick.jdbc.PostgresProfile.api._
 /**
   * Created by gcrowell on 2017-05-24.
   */
-//http://slick.lightbend.com/doc/3.2.0/schemas.html#mapped-tables
-case class StockOHLCRecord(symbol: String,
-                           dateId: Int,
-                           open: Double,
-                           high: Double,
-                           low: Double,
-                           close: Double,
-                           volume: Long,
-                           adjusted_close: Double
-                          ) extends Asset {
-  override def ticker: String = symbol
-}
 
 
 // Definition of the stock table (always use lowercase and _)
 class Stock(tag: Tag) extends Table[(String, String, Option[String], Option[String], Option[Int])](tag, Some("dim"), "stock") {
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = (symbol, name, currency, exchange, nasdaq_id)
+  def * = (ticker, name, currency, exchange, nasdaq_id)
 
-  def symbol = column[String]("symbol", O.PrimaryKey) // This is the primary key column
+  def ticker = column[String]("ticker", O.PrimaryKey) // This is the primary key column
 
   def name = column[String]("company_name") // follows any standard?
 
@@ -38,9 +26,22 @@ class Stock(tag: Tag) extends Table[(String, String, Option[String], Option[Stri
   def nasdaq_id = column[Option[Int]]("nasdaq_id") // used to get financial statement data (eg http://fundamentals.nasdaq.com/nasdaq_fundamentals.asp?CompanyID=8244&NumPeriods=50&Duration=1&documentType=1)
 }
 
+
+//http://slick.lightbend.com/doc/3.2.0/schemas.html#mapped-tables
+case class StockOHLCRecord(val ticker: String,
+                           val dateId: Int,
+                           val open: Double,
+                           val high: Double,
+                           val low: Double,
+                           val close: Double,
+                           val volume: Long,
+                           val adjusted_close: Double
+                          ) extends Asset {
+}
+
 // Definition of the stock_ohlc table
 class StockOHLC(tag: Tag) extends Table[StockOHLCRecord](tag, Some("fact"), "stock_ohlc") {
-  def * = (stock_symbol, date_id, open, high, low, close, volume, adjusted_close) <> (StockOHLCRecord.tupled, StockOHLCRecord.unapply)
+  def * = (ticker, date_id, open, high, low, close, volume, adjusted_close) <> (StockOHLCRecord.tupled, StockOHLCRecord.unapply)
 
   def open = column[Double]("open")
 
@@ -57,9 +58,9 @@ class StockOHLC(tag: Tag) extends Table[StockOHLCRecord](tag, Some("fact"), "sto
 
   def adjusted_close = column[Double]("adjusted_close")
 
-  def pk = primaryKey("pk_stockohlc", (stock_symbol, date_id))
+  def pk = primaryKey("pk_stockohlc", (ticker, date_id))
 
-  def stock_symbol = column[String]("stock_symbol")
+  def ticker = column[String]("ticker")
 
   def date_id = column[Int]("date_id")
 
@@ -68,7 +69,6 @@ class StockOHLC(tag: Tag) extends Table[StockOHLCRecord](tag, Some("fact"), "sto
 object Schema {
   val stock = TableQuery[Stock]
   val stockohlc = TableQuery[StockOHLC]
-  // TODO: fix so both tables can be created at same time
   // create table(s)
   val setup = DBIO.seq((stock.schema ++ stockohlc.schema).create)
 
